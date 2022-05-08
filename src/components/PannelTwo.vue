a7d557860ba3b2045cc05929c93242bed6cdb07e
xinglovepeng2019 <505963847@qq.com>
2022-05-08 21:42:05 +0800

    插槽

---
+++ src/components/PannelTwo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+      <span class="btn" @click="isShow = !isShow">
+        {{ isShow ? '收起' : '展开' }}
+      </span>
+    </div>
+    <div class="container" v-show="isShow">
+      <!--1： slot上绑定自定义属性和子组件内的值 -->
+      <slot name="content" tit="hello" :list="arr">默认内容</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+      arr: [
+        {
+          id: 1,
+          name: '寒雨连江夜入吴',
+        },
+        {
+          id: 2,
+          name: '平明送客楚山孤。',
+        },
+        {
+          id: 3,
+          name: '洛阳亲友如相问，',
+        },
+        {
+          id: 4,
+          name: '一片冰心在玉壶。',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
